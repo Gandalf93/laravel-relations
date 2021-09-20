1aaaa66ee8defc3701daad082ed65797ebc561a9
Giulia <giuliatullia93@gmail.com>
2021-09-20 18:32:29 +0200

    relations

--- app/Word.php
+++ app/Word.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    //
+    protected $table = 'words';
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'content'
+    ];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
